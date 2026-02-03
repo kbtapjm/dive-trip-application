0f030e81991d2d7c1a55570a1957742604b71787
jmpark <kbtapjm@gmail.com>
2026-02-03 17:10:33 +0900

    #1 flyway 적용

---
+++ src/main/resources/db/migration/V20260202__init.sql
@@ -0,0 +1,9 @@
+-- 스키마 생성에 대한 체크
+-- CREATE DATABASE IF NOT EXISTS `test`;
+-- USE `test`;
+
+-- 테이블 생성에 대한 체크
+CREATE TABLE IF NOT EXISTS `tb_test` (
+   `id` int(11) NOT NULL auto_increment,
+   PRIMARY KEY  (`id`)
+);
